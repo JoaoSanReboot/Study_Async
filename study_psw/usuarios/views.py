aa28a0c0b540511ea0e4513281b4ef9fca364605
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-17 13:32:21 -0300

    Cadastro.exe

--- study_psw/usuarios/views.py
+++ study_psw/usuarios/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
 
     if request.method == "GET":
          return render(request, 'cadastro.html')
     elif request.method == "POST":
-         return HttpResponse("Alo")
+         username = request.POST.get('username')
+         senha = request.POST.get('senha')
+         confirmar_senha = request.POST.get('confirmar_senha')
+
+         if not senha == confirmar_senha:
+              messages.add_message(request, constants.ERROR, 'Senha e confirmar senha não coincidem')
+              return redirect('/usuarios/cadastro')
+         
+         user = User.objects.filter(username=username)
+
+         if user.exists():
+              messages.add_message(request, constants.ERROR, 'Usuario já existe')
+              return redirect('/usuarios/cadastro')
+         
+         try:    
+               User.objects.create_user(
+                    username=username,
+                    password=senha
+               )
+               return redirect('/usuarios/login')#error
+         except:
+              messages.add_message(request, constants.ERROR, 'Error interno do servidor')
+              return redirect('/usuarios/cadastro')
