88bdfade1c5b15bffccf7e79b604eea0482c5710
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-16 21:15:34 -0300

    Projeto.StudyAsync

---
+++ study_psw/usuarios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def cadastro(request):
+
+    if request.method == "GET":
+         return render(request, 'cadastro.html')
+    elif request.method == "POST":
+         return HttpResponse("Alo")
